22ba19ccb5820b2f1088a6f7c8e8d1a05fab9be6
Jean Fagundes <jean-cabral@hotmail.com>
2022-03-21 01:23:32 -0300

    Adicionado autenticação de usuario e validações de cadastro

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -2,16 +2,16 @@ const User = require('../Models/User')
 const fs = require('fs')
 
 
-module.exports = class MenuController{
+module.exports = class MenuController {
 
-static async showMenu(req,res){
+        static async showMenu(req, res) {
 
-        const data = fs.readFileSync(`decks/yugi/magoNegro.json`, 'utf-8')
-        const card = JSON.parse(data)
-        
-        console.log(card)
-        
-        res.send(card)
-}
+                const data = fs.readFileSync(`decks/yugi/magoNegro.json`, 'utf-8')
+                const card = JSON.parse(data)
+
+                console.log(card)
+
+                res.send(card)
+        }
 
 }
